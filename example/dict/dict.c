f07da2138e1c789a165c5b6c7e6829be3af3ebec
qiongtubao <810776886@qq.com>
2019-03-10 12:31:22 +0800

    添加字典删除

--- example/dict/dict.c
+++ example/dict/dict.c
@@ -14,6 +14,7 @@ static void _dictReset(dictht *ht);
 static uint8_t dict_hash_function_seed[16];
 static void _dictRehashStep(dict *d);
 static unsigned long _dictNextPower(unsigned long size);
+static dictEntry *dictGenericDelete(dict *d, const void *key, int nofree);
 
 void _serverAssert(const char *estr, const char *file, int line) {
     fprintf(stderr, "=== ASSERTION FAILED ===");
@@ -279,4 +280,42 @@ dictEntry *dictFind(dict *d, const void *key) {
     }
     return NULL;
 }
+static dictEntry *dictGenericDelete(dict *d, const void *key, int nofree) {
+    uint64_t h, idx;
+    dictEntry *he, *prevHe;
+    int table;
 
+    if (d->ht[0].used == 0 && d->ht[1].used == 0) return NULL;
+
+    if (dictIsRehashing(d)) _dictRehashStep(d);
+    h = dictHashKey(d, key);
+
+    for (table = 0; table <= 1; table++) {
+        idx = h & d->ht[table].sizemask;
+        he = d->ht[table].table[idx];
+        prevHe = NULL;
+        while(he) {
+            if (key==he->key || dictCompareKeys(d, key, he->key)) {
+                /* Unlink the element from the list */
+                if (prevHe)
+                    prevHe->next = he->next;
+                else
+                    d->ht[table].table[idx] = he->next;
+                if (!nofree) {
+                    dictFreeKey(d, he);
+                    dictFreeVal(d, he);
+                    zfree(he);
+                }
+                d->ht[table].used--;
+                return he;
+            }
+            prevHe = he;
+            he = he->next;
+        }
+        if (!dictIsRehashing(d)) break;
+    }
+    return NULL; /* not found */
+}
+int dictDelete(dict *ht, const void *key) {
+    return dictGenericDelete(ht,key,0) ? DICT_OK : DICT_ERR;
+}
